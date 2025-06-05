05f2ad350d165831978b96162eb4527f79228c87
Juan Abraham Dominguez Loera <L22550292@chihuahua2.tecnm.mx>
2025-06-04 18:21:00 -0600

    Add files via upload

--- EVA2_3_LINKED_LIST.java
+++ EVA2_3_LINKED_LIST.java
@@ -2,9 +2,38 @@ package EVA2;
 
 public class EVA2_3_LINKED_LIST {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        
+       /* int datos[] = new int[10000];
+        long ini = System.nanoTime();
+        for (int i = 0; i < 10000; i++){
+            datos[i] = (int)(Math.random() * 1000);
+        }
+        long fin = System.nanoTime();
+        long tiempo = fin - ini;
+        System.out.println("Tiempo arreglo: " + tiempo);
+        
+        Lista lista = new Lista();
+        long ini1 = System.nanoTime();
+        //O(N2)
+        for (int i = 0; i < 10000; i++){
+            lista.add((int)(Math.random() * 1000));
+        }
+        long fin1 = System.nanoTime();
+        long tiempo1 = fin1 - ini1;
+        System.out.println("Tiempo arreglo: " + tiempo1);
+        System.out.println("Tamaño de la lista: " + lista.size()); */
+       Lista lista = new Lista();
+       lista.add(100);
+       lista.add(200);
+       lista.add(300);
+       lista.add(400);
+       lista.add(500);
+       lista.printList();
+        System.out.println("");
+        System.out.println("Cantidad de nodos " + lista.size());
+        try{
+            lista.inserAt(3, 1);
+        }
+    }
 }
